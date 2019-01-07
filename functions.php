370887a998d554eb5f7c6b964cc9d21cdfd61550
drik <>
2019-01-07 18:07:02 +0000

    Widget support

--- functions.php
+++ functions.php
@@ -8,12 +8,17 @@ Timber::$autoescape = false;
 class Liliom extends Timber\Site {
     public function __construct() {
         add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+        add_action( 'widgets_init', array( $this, 'widget_awareness' ) );
         add_filter( 'timber_context', array( $this, 'add_to_context' ) );
     }
 
     public function theme_supports() {
         add_theme_support( 'title-tag' );
         add_theme_support( 'woocommerce' );
+    }
+
+    public function widget_awareness() {
+        register_sidebar(array('id' => 'liliom-sidebar-1'));
     }
 
     public function add_to_context( $context ) {
