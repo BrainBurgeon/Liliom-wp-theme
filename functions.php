69c84e6018661014ec478cf9393a9b9449903eba
drik <>
2019-01-07 18:34:29 +0000

    More WC fiddling

--- functions.php
+++ functions.php
@@ -7,18 +7,29 @@ Timber::$autoescape = false;
 
 class Liliom extends Timber\Site {
     public function __construct() {
-        add_action( 'after_setup_theme', array( $this, 'after_setup_liliom' ) );
+        add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
         add_action( 'widgets_init', array( $this, 'widget_awareness' ) );
         add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-    }
 
-    public function after_setup_liliom() {
-        add_theme_support( 'title-tag' );
-        add_theme_support( 'woocommerce' );
-
+        // WooCommerce
         remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
         remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
         remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+        add_action('woocommerce_before_main_content', array( $this, 'my_theme_wrapper_start' ), 10);
+        add_action('woocommerce_after_main_content', arraye( $this, 'my_theme_wrapper_end' ), 10);
+    }
+
+    public function my_theme_wrapper_start() {
+        echo '<section id="main">';
+    }
+
+    public function my_theme_wrapper_end() {
+        echo '</section>';
+    }
+
+    public function theme_supports() {
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'woocommerce' );
     }
 
     public function widget_awareness() {
