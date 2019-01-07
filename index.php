69abe3330c64b668104aef53e8235aec070bb7b4
drik <>
2019-01-07 12:30:28 +0000

    Code cleanup, added Cloudflare sse tag to hide some stuff from suspicious visitors

--- index.php
+++ index.php
@@ -3,10 +3,4 @@
 $context = Timber::get_context();
 $templates = array( 'index.twig' );
 
-if(isset($_GET['testing123'])) {
-	//var_export(get_stylesheet_directory_uri());
-	var_export($context);
-	exit;
-}
-
 Timber::render( $templates, $context );
