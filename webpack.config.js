66679260f4201d564c1fd9f8281a5195d4338dcf
drik <>
2019-01-10 14:14:33 +0000

    Updated styles, webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path');
+// const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const incstr = require('incstr');
+const nextId = incstr.idGenerator({alphabet:'abcefghijklmnopqrstuvwxyz0123456789', prefix:'l', suffix:''});
 
 var config = {
     mode: 'production',
@@ -21,7 +23,16 @@ var config = {
             use: [
                 MiniCssExtractPlugin.loader,
                 // "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
+                // "css-loader", // translates CSS into CommonJS
+                {
+                    loader: "css-loader",
+                    options: {
+                        // modules: true,
+                        // getLocalIdent: (context, localIdentName, localName, options) => {
+                        //     return nextId();
+                        // }
+                    }
+                },
                 "sass-loader" // compiles Sass to CSS, using Node Sass by default
             ]
         }, {
