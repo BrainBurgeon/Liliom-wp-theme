51689a17c4f7e53316f4d18dc717a2932b9ec179
drik <>
2019-01-29 20:35:15 +0000

    ...

--- fusionpay_checkout_form-pay.php
+++ fusionpay_checkout_form-pay.php
@@ -1,6 +1,12 @@
 <?php
 
+$meta = get_post_meta($order->id, 'tgpayqrcode');
+if( is_array( $meta ) && count( $meta ) == 1 ) {
+    $meta = $meta[0];
+}
+
 $context = Timber::get_context();
 $context['order'] = $order;
+$context['meta'] = $meta;
 
 Timber::render( array( 'fusionpay-alipay.twig' ), $context );
