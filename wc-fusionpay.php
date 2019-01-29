ca91161cbc1ba2c30a0a172dc1237b4df906aa7b
drik <>
2019-01-29 13:13:01 +0000

    use array

--- wc-fusionpay.php
+++ wc-fusionpay.php
@@ -87,8 +87,8 @@ function init_wc_fusionpay() {
             try {
                 $xml = simplexml_load_file($xml_url);
                 if ( $xml->is_success == 'T' ) {
-                    // $xml_array = $this->xml2array($xml);
-                    add_post_meta( $order_id, 'tgpayqrcode', $xml );
+                    $xml_array = $this->xml2array($xml);
+                    add_post_meta( $order_id, 'tgpayqrcode', $xml_array );
                 }
             } catch( Exception $e ) {
                 // damn
