6c1f0e76a89cd254a7f4ba3b2aa3f4bd03d86d02
drik <>
2019-01-29 17:21:40 +0000

    Update order status if alipay trade was successful

--- order-status.php
+++ order-status.php
@@ -28,8 +28,11 @@ try {
         $args['sign'] = md5( http_build_query( $args ) . $options['token'] );
         $api_url = $options['apiurl'] . 'tgpaycheck.php?' . http_build_query( $args );
         $api_response = simplexml_load_file($api_url);
-        echo '<pre>';
-        var_export($api_response);
+
+        if ( $api_response->is_success == 'T' && $api_response->trade_status == 'TRADE_SUCCESS' ) {
+            $order->payment_complete( $api_response->alipay_trans_id );
+            $response['status'] = $order->get_status();
+        }
     }
 }
 catch( Exception $e ) {
