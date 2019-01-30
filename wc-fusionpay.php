4199b249cd2439fcf5b1cb0fef0fb75f346eaca6
drik <>
2019-01-30 11:56:10 +0000

    debug

--- wc-fusionpay.php
+++ wc-fusionpay.php
@@ -106,7 +106,7 @@ function init_wc_fusionpay() {
             
             try {
                 $api_response = simplexml_load_file( $xml_url );
-                var_export($api_response);exit;
+                var_export($api_response->result_code);exit;
                 if ( $api_response !== false && $api_response->result_code == 'SUCCESS' ) {
                     // $api_response_array = $this->xml2array( $api_response );
                     add_post_meta( $order_id, 'tgpayqrcode', $api_response );
