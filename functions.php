eb02dd9ca072380c3b4d7d926686bfcbdfec5a8c
AdrianMeszaros <47293295+AdrianMeszaros@users.noreply.github.com>
2019-02-16 19:52:26 +0000

    Custom taxonomy: Brands (with WC import support!)

--- functions.php
+++ functions.php
@@ -1,14 +1,19 @@
 <?php
 
-$translations = array( 'CN' => 'zh_CN' );
-
 // Timber is a must-use plugin, no need to check its existence
 
 Timber::$dirname = array( 'templates' );
 Timber::$autoescape = false;
 
 class Liliom extends Timber\Site {
+    private $domain = 'liliom';
+    private $translations = array( 'CN' => 'zh_CN' );
+    private $taxonomy_brand_name = 'product_brand';
+    private $taxonomy_brand_slug = 'brand';
+    private $taxonomy_term_id_cache = array();
+
     public function __construct() {
+        add_action( 'init', array( $this, 'register_taxonomy_brand' ) );
         add_filter( 'locale', array( $this, 'set_my_locale' ) );
         add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
         add_action( 'widgets_init', array( $this, 'widget_awareness' ) );
@@ -34,40 +39,94 @@ class Liliom extends Timber\Site {
         // Remove tabs from product detail page
         remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
-        // remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-        // remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-        // add_action('woocommerce_before_main_content', array( $this, 'my_theme_wrapper_start' ), 10);
-        // add_action('woocommerce_after_main_content', array( $this, 'my_theme_wrapper_end' ), 10);
-
         add_filter( 'woocommerce_output_related_products_args', array( $this, 'output_related_products_args' ), 9999 );
-
         remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
         add_action( 'woocommerce_shop_loop_item_title', array( $this, 'shop_loop_item_title' ) );
-
         add_filter( 'woocommerce_cart_item_name', array( $this, 'cart_item_name' ), 10, 3 );
-
         remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
         add_action( 'woocommerce_single_product_summary', array( $this, 'single_title' ), 5 );
+
+        // Extend WC Import with Brand
+        add_filter( 'woocommerce_csv_product_import_mapping_options', array( $this, 'csv_product_import_mapping_options' ) );
+        add_filter( 'woocommerce_product_importer_parsed_data', array( $this, 'product_importer_parsed_data' ), 10, 2 );
+        add_filter( 'woocommerce_product_import_inserted_product_object', array( $this, 'product_import_inserted_product_object' ), 10, 2 );
+        add_filter( 'woocommerce_csv_product_import_mapping_default_columns', array( $this, 'csv_product_import_mapping_default_columns' ) );
     }
 
-    // public function my_theme_wrapper_start() {
-    //     echo '<section id="main">';
-    // }
+    public function register_taxonomy_brand() {
+        $labels = array(
+            'name' => __( 'Brands', $this->domain ),
+            'singular_name' => __( 'Brand', $this->domain ),
+            'add_new_item' => __( 'Add New Brand', $this->domain )
+        );
 
-    // public function my_theme_wrapper_end() {
-    //     echo '</section>';
-    // }
+        $args = array(
+            'hierarchical' => true,
+            'labels' => $labels,
+            'show_ui' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+            'rewrite' => array( 'slug' => $this->taxonomy_brand_slug )
+        );
 
-    public function set_my_locale($lang) {
-        global $translations;
+        register_taxonomy( $this->taxonomy_brand_name, array( 'product' ), $args );
+    }
+
+
+
+    // Based on https://gist.github.com/helgatheviking/114c8df50cabb7119b3c895b1d854533
+
+    public function csv_product_import_mapping_options( $columns ) {
+        $columns[ $this->taxonomy_brand_name ] = __( 'Brand', $this->domain );
+        return $columns;
+    }
+
+    public function csv_product_import_mapping_default_columns( $columns ) {
+        $columns[ 'Brand' ] = $this->taxonomy_brand_name;
+        return $columns;
+    }
+
+    public function product_importer_parsed_data( $parsed_data, $importer ) {
+        if ( empty( $parsed_data[ $this->taxonomy_brand_name ] ) ) {
+            return $parsed_data;
+        }
+
+        $data = explode( ',', $parsed_data[ $this->taxonomy_brand_name ] );
+        $data = array_map( 'trim', $data );
+        unset( $parsed_data[ $this->taxonomy_brand_name ] );
+
+        if ( is_array( $data ) ) {
+            $parsed_data[ $this->taxonomy_brand_name ] = array();
+
+            foreach( $data as $term_name ) {
+                $parsed_data[ $this->taxonomy_brand_name ][] = $this->get_or_create_term_id( $term_name );
+            }
+        }
+
+        return $parsed_data;
+    }
+
+    public function product_import_inserted_product_object( $product, $data ) {
+        if ( is_a( $product, 'WC_Product' ) ) {
+            if ( ! empty( $data[ $this->taxonomy_brand_name ] ) ) {
+                wp_set_object_terms( $product->get_id(),  array_values( (array) $data[ $this->taxonomy_brand_name ] ), $this->taxonomy_brand_name );
+            }
+        }
+
+        return $product;
+    }
+
+
+
+    public function set_my_locale( $lang ) {
         $lang_key = '';
         if ( !empty( $_GET['language'] ) ) {
             $lang_key = $_GET['language'];
         } else if ( !empty( $_SERVER['HTTP_CF_IPCOUNTRY'] ) ) {
             $lang_key = $_SERVER['HTTP_CF_IPCOUNTRY'];
         }
-        if ( !empty( $translations[$lang_key] ) ) {
-            return $translations[$lang_key];
+        if ( !empty( $this->translations[$lang_key] ) ) {
+            return $this->translations[$lang_key];
         }
         return $lang;
     }
@@ -78,14 +137,14 @@ class Liliom extends Timber\Site {
     }
 
     public function widget_awareness() {
-        register_sidebar(array('id' => 'liliom-sidebar-1'));
+        register_sidebar( array( 'id' => 'liliom-sidebar-1' ) );
     }
 
     public function add_to_context( $context ) {
         if ( !empty( $_SERVER['HTTP_CF_IPCOUNTRY'] ) ) {
             $context['CF_IPCOUNTRY'] = $_SERVER['HTTP_CF_IPCOUNTRY'];
         }
-	    $context['menu'] = new Timber\Menu('Main Menu');
+	    $context['menu'] = new Timber\Menu( 'Main Menu' );
 	    $context['site'] = new TimberSite();
 	    return $context;
     }
@@ -97,21 +156,26 @@ class Liliom extends Timber\Site {
     }
 
     public function shop_loop_item_title() {
-        $title = $this->processTitle( get_the_title() );
+        $title = $this->process_title( get_the_title() );
         echo '<h3 class="brand-name">' . $title['brand_name'] . '</h3><h2 class="product-name">' . $title['product_name'] . '</h2>' . '<span class="product-info">' . $title['info'] . '</span>';
     }
 
     public function cart_item_name( $title, $item, $key ) {
-        $name = $this->processTitle( $title );
+        $name = $this->process_title( $title );
         return '<div class="the-product"><div class="the-product-name">' .  $name['product_name'] . '</div><div class="the-brand-name">' . strip_tags($name['brand_name']) . '</div><div class="the-info">' . $name['info'] . '</div></div>';
     }
 
     public function single_title() {
-        $title = $this->processTitle( get_the_title() );
-        echo '<h1 class="product-name">' . $title['product_name'] . '</h1><div class="brand-name">' . $title['brand_name'] . '</div>';
+        $title = $this->process_title( get_the_title() );
+        $brand = $title['brand_name'];
+        if ( !empty( $title['brand_name'] ) ) {
+            $taxonomy_url = get_home_url( null, 'brand/' . sanitize_title( $title['brand_name'] ) . '/' );
+            $brand = '<a href="' . $taxonomy_url . '">' . $title['brand_name'] . '</a>';
+        }
+        echo '<h1 class="product-name">' . $title['product_name'] . '</h1><div class="brand-name">' . $brand . '</div>';
     }
 
-    private function processTitle( $title ) {
+    private function process_title( $title ) {
         $processed = array(
             'brand_name' => '',
             'product_name' => $title,
@@ -127,6 +191,19 @@ class Liliom extends Timber\Site {
         }
         
         return $processed;
+    }
+
+    private function get_or_create_term_id( $term_name ) {
+        if ( empty( $this->taxonomy_term_id_cache[ $term_name ] ) ) {
+            $term = get_term_by( 'name', $term_name, $this->taxonomy_brand_name, ARRAY_A );
+            if ( empty( $term ) ) {
+                // Create a new term
+                $term = wp_insert_term( $term_name, $this->taxonomy_brand_name );
+            }
+            $this->taxonomy_term_id_cache[ $term_name ] = (int) $term[ 'term_id' ];
+        }
+        
+        return $this->taxonomy_term_id_cache[ $term_name ];
     }
 }
 
